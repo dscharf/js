622ea0986136e0f036cc1aeb18d402fdf4df39cc
dscharf <dominik.scharf@gmx.de>
2013-11-02 21:06:43 +0100

    02

---
+++ partition/linear.js
@@ -0,0 +1,19 @@
+
+var seq = [9, 2, 6, 3, 8, 5, 8, 1, 7, 3, 4];
+
+function linearPartition(seq, k) {
+    alert(seq.length);
+/*
+    var n = seq.length;
+
+    if (k <= 0)
+        return [];
+    if (k < n)
+        return seq;
+
+    var int[][] table = new int[k][n];
+    var int[][] solution = new int[k - 1][n - 1];
+
+    //for (var i = 0; i < table.
+*/          
+}
